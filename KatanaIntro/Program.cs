e325698743ce5b50f3b4930d8797dc89f9240c79
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-11-04 13:00:24 +0200

    OWIN and Katana. The AppFunc

--- KatanaIntro/Program.cs
+++ KatanaIntro/Program.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.Owin.Hosting;
 using Owin;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace KatanaIntro
 {
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
     class Program
     {
         static void Main(string[] args)
@@ -23,12 +28,33 @@ namespace KatanaIntro
     {
         public void Configuration(IAppBuilder app)
         {
-            app.UseWelcomePage();
+            app.UseHelloWorld();
+        }
+    }
 
-            //app.Run(ctx =>
-            //{
-            //    return ctx.Response.WriteAsync("Hello World!");
-            //});
+    public static class AppBuilderExtensions
+    {
+        public static void UseHelloWorld(this IAppBuilder app)
+        {
+            app.Use<HelloWorldComponent>();
+        }
+    }
+
+    public class HelloWorldComponent
+    {
+        private AppFunc _next;
+        public HelloWorldComponent(AppFunc next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(IDictionary<string, object> environment)
+        {
+            var response = environment["owin.ResponseBody"] as Stream;
+            using (var writer = new StreamWriter(response))
+            {
+                return writer.WriteAsync("Hello!");
+            }
         }
     }
 }
